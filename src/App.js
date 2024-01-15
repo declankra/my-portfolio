fa56a00b98770b7d15bbcefce22d2d7361902b62
declankra <declankramper@gmail.com>
2024-01-15 16:06:43 -0600

    fixed router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar'; // Import the Navbar component
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
 import TagManager from 'react-gtm-module'
 // Add google tag manager for GUA 4
@@ -9,10 +14,17 @@ const tagManagerArgs = {
 TagManager.initialize(tagManagerArgs)
 
 function App() {
-  return ( 
-    <div>
-      
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
     </div>
+
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,16 @@ import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
 
-const Navbar = () => {
+function Navbar() {
   return (
-    <div className="navbar">
-    
-    </div>
+      <nav>
+          <ul>
+              <li><NavLink to="/">Home</NavLink></li>
+              <li><NavLink to="/projects">Projects</NavLink></li>
+              <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
+      </nav>
   );
-};
+}
 
 export default Navbar;
