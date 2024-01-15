2e28ef6b2a94d98532cb665512a328fd05aa4c22
declankra <declankramper@gmail.com>
2024-01-15 14:10:09 -0600

    added sharedStyles.js

---
+++ src/sharedStyles.js
@@ -0,0 +1,30 @@
+export const pageH2Style = {
+    color: 'blue',
+    fontFamily: 'arial',
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: '2em' // Assuming h2 to be the largest
+  };
+  
+  export const pageH3Style = {
+    color: 'orange',
+    fontFamily: 'arial',
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: '1.5em' // Slightly smaller than h2
+  };
+  
+  export const pageH4Style = {
+    color: 'purple', // Changed for distinction
+    fontFamily: 'arial',
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: '1.2em' // Smaller than h3
+  };
+  
+  export const pStyle = {
+    color: 'black',
+    fontFamily: 'arial',
+    // Add a font size if needed for the paragraph, typically smaller than h4.
+  };
+  
