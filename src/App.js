85847eca422f428cf74de580cde4ed0c7a7e9178
declankra <declankramper@gmail.com>
2024-01-15 14:41:45 -0600

    routes instead of switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -12,11 +12,11 @@ function App() {
     <Router>
       <div className="App">
         <Navbar />
-        <Switch>
+        <Routes>
           <Route path="/" exact component={Home} />
           <Route path="/projects" component={Projects} />
           <Route path="/contact" component={Contact} />
-        </Switch>
+        </Routes>
         <Footer />
       </div>
     </Router>
