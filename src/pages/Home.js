92ed471351065262c7dd20ee942b42a92180f145
declankra <declankramper@gmail.com>
2024-01-17 20:02:34 -0600

    continued homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,32 @@
 import React from "react";
 
-const Home = () => {
+
+const Avatar = () => {
+  const avatarPath = process.env.PUBLIC_URL + 'Avatar_Me_PS_Blue.png'
+  return(
+    <img classNAme="avatar"
+      src={avatarPath}
+      alt={'Declan Kramper Profile Picture'}
+      style = {{
+        resizeMode: 'center',
+        width: 125,
+        height: 125
+      }}
+      />
+  )
+}
+
+const nameStyle = {
+  fontWeight: "bold",
+}
+
+function Home() {
     return (
-      <div>
-        <p>home</p>
+      <div className="Home">
+          <div className="header">
+            <h1 style={nameStyle}>Declan Kramper</h1>
+            <Avatar />
+          </div>
       </div>
     );
   };
