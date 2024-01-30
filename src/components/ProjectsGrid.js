b688082c1049fd8c4a37543274e284f4050a377c
declankra <declankramper@gmail.com>
2024-01-30 16:48:38 -0600

    added magic record player project

--- src/components/ProjectsGrid.js
+++ src/components/ProjectsGrid.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import { Grid } from '@mui/material';
 import ProjectCard from './ProjectCard';
+//import recordPlayerImage from '../images/recordPlayer.jpeg'; // Adjust the path as per your directory structure
+
 
 const ProjectsGrid = () => {
   // Example data - this could come from a database or API in a real-world scenario
   const projects = [
     {
-      imageUrl: 'image_url_1.jpg',
-      title: 'Project One',
-      what: 'Description of what project one is.',
-      how: 'Explanation of how project one works.',
+      imageUrl: require(`../images/recordPlayer.jpeg`),
+      title: 'Magic Record Player',
+      what: 'Magically play the soundtrack of your memories.',
+      how: 'A raspberry pi running a python script to interact with NFC tagged photos',
       why: 'Reason why project one exists.',
-      url: 'https://example.com/project1'
+      url: 'https://declankramper.notion.site/NFC-Record-Player-Build-bf95a606cc474c11a626b50821fb12d4?pvs=4'
     },
+    {
+        imageUrl: require(`../images/recordPlayer.jpeg`),
+        title: 'Magic Record Player',
+        what: 'Magically play the soundtrack of your memories.',
+        how: 'A raspberry pi running a python script to interact with NFC tagged photos',
+        why: 'Reason why project one exists.',
+        url: 'https://declankramper.notion.site/NFC-Record-Player-Build-bf95a606cc474c11a626b50821fb12d4?pvs=4'
+      },
     // ... more projects
   ];
 
   return (
-    <Grid container spacing={2}>
+    <Grid container spacing={4}>
       {projects.map((project, index) => (
         <ProjectCard key={index} {...project} />
       ))}
