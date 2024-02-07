18576a32b48763eb4a7b1fc45482ba478916d415
declankra <declankramper@gmail.com>
2024-02-07 17:54:14 -0600

    added health page

---
+++ src/pages/Health.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './Health.css'
+
+
+
+
+
+
+function Health() {
+    return (
+        <main>
+            <h1>Contact</h1>
+            <p>hello world</p>
+            <Resume />
+        </main>
+    )
+}
+
+export default Health;
