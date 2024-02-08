7712e82cb7437a7d7c8a33e179631210fd8d818e
declankra <declankramper@gmail.com>
2024-02-07 19:37:17 -0600

    edited

--- src/pages/Health.js
+++ src/pages/Health.js
@@ -5,10 +5,6 @@ const iframeHtml = {
     __html: `<iframe width="700" height="500" src="https://lookerstudio.google.com/embed/reporting/8667b22a-1248-4f1b-a3f3-8cbc158f00a6/page/3FkpD" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>`
 };
 
-const dashboardButton = () => {
-
-}
-
 function Health() {
     return (
         <main>
