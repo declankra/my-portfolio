4f54094cb04e91acb151cbdc363aefe44f6f727a
declankra <declankramper@gmail.com>
2024-02-13 16:55:04 -0600

    finished goals page

--- src/pages/Goals.js
+++ src/pages/Goals.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material";
 
 // Define the URL of your Google Cloud Function
 const cloudFunctionUrl = "https://us-central1-my-portfolio-405919.cloudfunctions.net/getGoalSheetData";
@@ -22,33 +23,55 @@ const GoalsComponent = () => {
 
     useEffect(() => {
         getGoalMetrics().then(data => {
-            console.log(data); // Check the fetched data
             setGoalMetrics(data);
         });
     }, []);
 
+    // Predefined goal descriptions
+    const goalDescriptions = [
+        "Operate within “feel best” range for 80% of days",
+        "Block out 4-6 hours per week to build something I enjoy",
+        "Read 6-12 books",
+    ];
+
     return (
-        <div>
-            <h2>Goal Metrics</h2>
-            {goalMetrics.length > 0 ? (
-                goalMetrics.map((metric, index) => (
-                    <div key={index}>
-                        <h3>{metric.sheetName}</h3>
-                        <pre>{JSON.stringify(metric.data, null, 2)}</pre>
-                    </div>
-                ))
-            ) : (
-                <p>No data found</p>
-            )}
-        </div>
+        <TableContainer component={Paper}style={{ marginBottom: 20, marginTop: 20 }}>
+            <Table aria-label="goal metrics table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell style={{ fontWeight: 'bold' }}>Goal Description</TableCell>
+                        <TableCell align="right" style={{ fontWeight: 'bold' }}>Current #</TableCell>
+                        <TableCell align="right" style={{ fontWeight: 'bold' }}>Goal #</TableCell>
+                        <TableCell align="right" style={{ fontWeight: 'bold' }}>Progress %</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {goalMetrics.map((metric, index) => (
+                        <TableRow key={index}>
+                            <TableCell component="th" scope="row">
+                                {goalDescriptions[index]}
+                            </TableCell>
+                            <TableCell align="right">{metric.data ? metric.data[1][0] : 'N/A'}</TableCell> {/* Current # */}
+                            <TableCell align="right">{metric.data ? metric.data[1][1] : 'N/A'}</TableCell> {/* Goal # */}
+                            <TableCell align="right">{metric.data ? metric.data[1][2] : 'N/A'}</TableCell> {/* Progress % */}
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     );
 };
+
+
 
 function Goals() {
     return (
         <main>
             <h1>Goals</h1>
             <GoalsComponent />
+            <p style={{ marginTop: '20px' }}>
+                To see how this table works, go to the <a href="https://declankramper.notion.site/Visual-Goal-Tracker-e93fdab0cad1446f896983068aa90a4f?pvs=4" target="_blank" rel="noopener noreferrer">Notion build page</a>.
+            </p>
         </main>
     );
 }
