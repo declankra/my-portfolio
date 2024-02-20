38c9c26a4440d0b612444bb96e45f78195974681
declankra <declankramper@gmail.com>
2024-02-20 16:10:15 -0600

    added homepage welcome message

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,15 +4,15 @@ import './Home.css'
 
 const Avatar = () => {
   const avatarPath = process.env.PUBLIC_URL + 'Avatar_ME_PS.jpeg'
-  return(
+  return (
     <img className="avatar"
       src={avatarPath}
       alt={'Declan Kramper Profile'}
-      style = {{
+      style={{
         width: 125,
         height: 125
       }}
-      />
+    />
   )
 }
 
@@ -35,11 +35,23 @@ const Profile = () => {
 };
 
 function Home() {
-    return (
-      <main>
-          <Profile/>
-      </main>
-    );
-  };
-  
-  export default Home;
+  return (
+    <main>
+      <section className="hero">
+        <Profile />
+      </section>
+      <section className="body">
+        <div className="welcome-message">
+          <h2>Hi!</h2>
+          <p>
+            Welcome to my space on the internet. I enjoy learning about and (moreso lately) building products. This website is one of those products - it'll always be a WIP as I continually add newly completed <a href="#projects">projects</a>, thoughts on the <a href="#blogs">blog</a> (coming soon), polishing touches. <br></br><br></br>Ultimately, I hope to positively impact others through one of my own products. <br></br><br></br> The date in the footer keeps me honest - check back soon for new developments!
+          </p>
+        </div>
+      </section>
+
+      {/* Rest of the homepage content */}
+    </main>
+  );
+};
+
+export default Home;
