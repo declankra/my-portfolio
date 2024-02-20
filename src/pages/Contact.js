e643e7a3fe30884528715bd17c841079edf85137
declankra <declankramper@gmail.com>
2024-02-20 15:18:42 -0600

    added dual resumes

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,22 +1,21 @@
-import React from "react";
-import './Contact.css'
+import React, { useState } from 'react';
+import './Contact.css';
 
-const Resume = () => {
-    const ResumePath = process.env.PUBLIC_URL + 'DeclanKramper_Resume_PM.pdf' // PM resume
+const Resume = ({ title, subtitle, path }) => {
     return (
         <div className="resume-container">
             <div className="resume-header">
-                <h2 className="resume-title">Product Development Resume</h2>
-                <p className="resume-subtitle">last updated: June 2023</p>
+                <h2 className="resume-title">{title}</h2>
+                <p className="resume-subtitle">last updated: {subtitle}</p>
             </div>
             <div className="pdf-container">
                 <object
-                    data={ResumePath}
+                    data={path}
                     type="application/pdf"
                     width="100%"
                     height="100%">
                     <p>Your viewport is too small.
-                        <a href="/DeclanKramper_Resume_05082023.pdf">Download the PDF</a>.</p>
+                        <a href={path}>Download the PDF</a>.</p>
                 </object>
             </div>
         </div>
@@ -24,13 +23,38 @@ const Resume = () => {
 };
 
 function Contact() {
+    const [selectedResume, setSelectedResume] = useState('PM'); // PM for Product Management, TD for Technical Development
+    const resumes = {
+        PM: {
+            title: "Product Development Resume",
+            subtitle: "June 2023",
+            path: process.env.PUBLIC_URL + 'DeclanKramper_Resume_PM.pdf',
+        },
+        TD: {
+            title: "Delivery Analyst Resume",
+            subtitle: "Feb 2024",
+            path: process.env.PUBLIC_URL + 'DeclanKramper_Resume_DeliveryAnalyst.pdf',
+        }
+    };
+    const buttonStyle = {
+        background: 'white',
+        color: 'blue',
+        border: 'none',
+        cursor: 'pointer',
+        padding: '5',
+    };
+
     return (
         <main>
             <h1>Contact</h1>
             <p>hello world</p>
-            <Resume />
+            <div className="resume-switch" style={{ textAlign: 'center'}}>
+                <button style={buttonStyle} onClick={() => setSelectedResume('PM')}>Product Development Resume</button>
+                <button style={buttonStyle} onClick={() => setSelectedResume('TD')}>Delivery Analyst Resume</button>
+            </div>
+            <Resume {...resumes[selectedResume]} />
         </main>
-    )
+    );
 }
 
 export default Contact;
