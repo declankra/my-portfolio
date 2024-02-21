6c3cfea16c1166ba71c0431ce9dac1a1d50a072e
declankra <declankramper@gmail.com>
2024-02-20 20:53:22 -0600

    imported and added global fonts

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import { createRoot } from 'react-dom/client'; // Import createRoot for React v1
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+   families: [
+      'Inconsolata:200,400', 
+      'Roboto:100,400',
+      'Poppins:400,600,700',
+      'Titillium Web:200,400,600,700',
+      'Space Grotesk:300,4000,600,700',
+      'sans-serif'
+    ] // applied globally in index.css file
+
+  }
+});
 
 const root = createRoot(document.getElementById('root')); // Use createRoot
 root.render(
