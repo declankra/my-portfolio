40e8382107fdea88f549a2bdb7d0810b2aaf45b5
declankra <declankramper@gmail.com>
2024-03-06 12:24:40 -0600

    updated date

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 function Footer() {
   return (
     <div className="Footer">
-      <p>website was last updated: 3/01/24</p>
+      <p>website was last updated: 3/06/24</p>
       <p>see github repo for online portfolio updates: <a href="https://github.com/declankra/my-portfolio">declankra/my-portfolio</a> </p> 
     </div>
   );
