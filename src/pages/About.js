e2c4733bc1ee966d4fe0996551b5b2d7b9e41c47
declankra <declankramper@gmail.com>
2024-03-06 12:24:11 -0600

    added material UI linkedin and github icons to About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import './About.css';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
 
 const ResumeViewer = ({ title, subtitle, path }) => (
     <div className="resume-container">
@@ -33,6 +36,16 @@ function About() {
     return (
         <main>
             <section className='hero'>
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '20px' }}>
+                    {/* LinkedIn Icon */}
+                    <a href="https://www.linkedin.com/in/declan-kramper" target="_blank" rel="noopener noreferrer" style={{ marginRight: '10px' }}>
+                        <LinkedInIcon sx={{ fontSize: 45, color: 'blue' }} />
+                    </a>
+                    {/* GitHub Icon */}
+                    <a href="https://github.com/declankra" target="_blank" rel="noopener noreferrer">
+                        <GitHubIcon sx={{ fontSize: 40, color: 'black' }} />
+                    </a>
+                </div>
                 <h1>About</h1>
                 <p>hello world. page is under construction. for now, here's some resumes.</p>
             </section>
