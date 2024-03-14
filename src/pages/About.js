af8c364c87aff32fd8ce818bcf068d7cdc09ee94
declankra <declankramper@gmail.com>
2024-03-14 18:02:53 -0500

    switched resume over to resume page

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,34 +4,9 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 
-const ResumeViewer = ({ title, subtitle, path }) => (
-    <div className="resume-container">
-        <div className="resume-header">
-            <h2 className="resume-title">{title}</h2>
-            <p className="resume-subtitle">last updated: {subtitle}</p>
-        </div>
-        <div className="pdf-container">
-            <object data={path} type="application/pdf" width="100%" height="100%">
-                <p>Your viewport is too small. <a href={path}>Download the PDF</a>.</p>
-            </object>
-        </div>
-    </div>
-);
 
 function About() {
-    const [selectedResume, setSelectedResume] = useState('PM');
-    const resumes = {
-        PM: {
-            title: "Product Development Resume",
-            subtitle: "June 2023",
-            path: process.env.PUBLIC_URL + '/DeclanKramper_Resume_PM.pdf',
-        },
-        TD: {
-            title: "Delivery Analyst Resume",
-            subtitle: "Feb 2024",
-            path: process.env.PUBLIC_URL + '/DeclanKramper_Resume_DeliveryAnalyst.pdf',
-        }
-    };
+  
 
     return (
         <main>
@@ -50,13 +25,6 @@ function About() {
                 <p>hello world. page is under construction. for now, here's some resumes.</p>
             </section>
             <section className='body'>
-                <div className='resume-section'>
-                    <div className="resume-switch">
-                        <button className="resume-button" onClick={() => setSelectedResume('PM')}>Product Development Resume</button>
-                        <button className="resume-button" onClick={() => setSelectedResume('TD')}>Delivery Analyst Resume</button>
-                    </div>
-                    <ResumeViewer {...resumes[selectedResume]} />
-                </div>
             </section>
         </main>
     );
---
+++ src/pages/Resume.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import './Resume.css'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+const ResumeViewer = ({ title, subtitle, path }) => (
+    <div className="resume-container">
+        <div className="resume-header">
+            <h2 className="resume-title">{title}</h2>
+            <p className="resume-subtitle">last updated: {subtitle}</p>
+        </div>
+        <div className="pdf-container">
+            <object data={path} type="application/pdf" width="100%" height="100%">
+                <p>Your viewport is too small. <a href={path}>Download the PDF</a>.</p>
+            </object>
+        </div>
+    </div>
+);
+
+
+function Resume() {
+
+    const [selectedResume, setSelectedResume] = useState('PM');
+    const resumes = {
+        PM: {
+            title: "Product Development Resume",
+            subtitle: "June 2023",
+            path: process.env.PUBLIC_URL + '/DeclanKramper_Resume_PM.pdf',
+        },
+        TD: {
+            title: "Delivery Analyst Resume",
+            subtitle: "Feb 2024",
+            path: process.env.PUBLIC_URL + '/DeclanKramper_Resume_DeliveryAnalyst.pdf',
+        }
+    };
+
+
+    return (
+        <main>
+            {/* Back Icon */}
+            <a href="https://www.declankramper.me/about" target="_blank" rel="noopener noreferrer" style={{ marginRight: '10px' }}>
+                <ArrowBackIcon sx={{ fontSize: 40, color: 'blue' }} />
+            </a>
+            <h1>Resume</h1>
+            <div className='resume-section'>
+                <div className="resume-switch">
+                    <button className="resume-button" onClick={() => setSelectedResume('PM')}>Product Development Resume</button>
+                    <button className="resume-button" onClick={() => setSelectedResume('TD')}>Delivery Analyst Resume</button>
+                </div>
+                <ResumeViewer {...resumes[selectedResume]} />
+            </div>
+        </main>
+    )
+}
+export default Resume;
