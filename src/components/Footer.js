d2252d432f2ceda1754b36a64950c91a86cd3b8e
declankra <declankramper@gmail.com>
2024-04-19 16:15:03 -0500

    dated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 function Footer() {
   return (
     <div className="Footer">
-      <p>website was last updated: 4/14/24</p>
+      <p>website was last updated: 4/19/24</p>
       <p>see github repo for online portfolio updates: <a href="https://github.com/declankra/my-portfolio"><GitHubIcon /> declankra/my-portfolio</a></p>
     </div>
   );
