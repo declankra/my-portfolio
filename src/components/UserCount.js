973cbfc4e2403cc2ce424d69fa791f7a38f84373
declankra <declankramper@gmail.com>
2024-10-07 09:53:10 -0500

    updated user count

--- src/components/UserCount.js
+++ src/components/UserCount.js
@@ -20,8 +20,8 @@ const colorPalette = {
 const data = [
   { id: 0, value: 4, label: 'Magic Record Player | IOT Controller', color: colorPalette[1] },
   { id: 1, value: 0, label: 'Soundcloud Downloader | OS Code', color: colorPalette[3] },
-  { id: 2, value: 55, label: 'Meet or Not | AI Web App', color: colorPalette[2] },
-  { id: 3, value: 460, label: 'Race Time Calculator | IOS App', color: colorPalette[0] },
+  { id: 2, value: 64, label: 'Meet or Not | AI Web App', color: colorPalette[2] },
+  { id: 3, value: 503, label: 'Race Time Calculator | IOS App', color: colorPalette[0] },
 ];
 
 const totalCount = data.reduce((sum, item) => sum + item.value, 0);
